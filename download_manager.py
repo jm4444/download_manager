7589a2be1457270e08d07909c690574b43cfd53f
Justin Meredith <jmeredith.4444@gmail.com>
2018-03-11 22:17:23 -0500

    Sorts all files into designated folders.

--- download_manager.py
+++ download_manager.py
@@ -7,6 +7,14 @@ for file in os.listdir("../Downloads"):
     if os.path.isdir("../Downloads/" + str(file)) == True:    # This skips directories
         continue
 
-    if file.lower().endswith(('.exe')):
+    if file.lower().endswith('.exe'):
         print("Installer: " + file)
         os.rename("../Downloads/" + file, "../Downloads/Installers/" + file)
+    elif file.lower().endswith(('.jpg', '.jpeg', '.png')) or file.lower().endswith(('.gif')):
+        os.rename("..Downloads/" + file, "../Downloads/Images/" + file)
+    elif file.lower().endswith(('.doc', '.docx', '.xls')) or file.lower().endswith(('.xlsx', '.ppt', '.pptx')):
+        os.rename("../Downloads/" + file, "../Downloads/MicrosoftOffice Files/" + file)
+    elif file.lower().endswith(('.pdf')):
+        os.rename("../Downloads/" + file, "../Downloads/PDFs/" + file)
+    else:
+        os.rename("../Downloads/" + file, "../Downloads/Misc/" + file)
