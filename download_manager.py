02c4b93069e03ec54c18c540f5f00001e1f48642
Justin Meredith <jmeredith.4444@gmail.com>
2018-03-11 21:43:04 -0500

    Removes use of the python-magic lib and instead checks the end of the file name to determine file type. The program can now move intallers into a designated folder.

--- download_manager.py
+++ download_manager.py
@@ -1,10 +1,12 @@
 # Download Manager
 # The purpose of this code is to automatically sort files in my Downloads folder
 
-import magic
 import os
 
-for file in os.listdir("../Dummy"):
-    print("\nFile Name: " + str(file) + "\n")
-    print(magic.from_file("../Dummy/" + str(file)))
-#print(magic.from_file("../Dummy/1997.jpg"))
+for file in os.listdir("../Downloads"):
+    if os.path.isdir("../Downloads/" + str(file)) == True:    # This skips directories
+        continue
+
+    if file.lower().endswith(('.exe')):
+        print("Installer: " + file)
+        os.rename("../Downloads/" + file, "../Downloads/Installers/" + file)
